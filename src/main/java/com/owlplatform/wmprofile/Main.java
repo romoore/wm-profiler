3f4628b94b6f6713511904ffc93f73d66ff5b545
Robert Moore <rob@owlplatform.com>
2014-08-14 00:30:13 -0400

    Handles specified experiments.
    + Only has "generator" side, no receiver yet.

--- src/main/java/com/owlplatform/wmprofile/Main.java
+++ src/main/java/com/owlplatform/wmprofile/Main.java
@@ -2,6 +2,7 @@ package com.owlplatform.wmprofile;
 
 import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.Collection;
 import java.util.Random;
 
 import com.owlplatform.worldmodel.Attribute;
@@ -20,6 +21,12 @@ public class Main {
 	private static final String ATTRIBUTE_FORMAT = "a%05d";
 	private static final String COUNT_FORMAT = "%,11d";
 	private static final String TIME_FORMAT = "%,15d";
+	private static final int SEARCH_ITERATIONS = 10;
+
+	private static final String REGEX_PATTERN_SIMPLE = ".*";
+	private static final String REGEX_PATTERN_MEDIUM = "^(.*?[a-z])(.*[0-9]){8}9";
+	private static final String REGEX_PATTERN_COMPLEX = "(([^\\.]*))*1";
+	private static final String REGEX_PATTERN_COMPLEX_HI_EMPTY = "(([^\\.]*))*p";
 
 	public static void main(String[] args) {
 		final String host = args[0];
@@ -28,6 +35,7 @@ public class Main {
 
 		int numAttributes = 0;
 		int numAttrPerId = 1;
+		boolean expire = false;
 
 		for (int i = 3; i < args.length; ++i) {
 			if ("--createAttributes".equalsIgnoreCase(args[i])) {
@@ -38,6 +46,9 @@ public class Main {
 					return;
 				}
 				numAttributes = Integer.parseInt(args[++i]);
+			} else if ("--expire".equalsIgnoreCase(args[i])) {
+				expire = true;
+				System.out.println("Expiring attributes.");
 			}
 		}
 		final SolverWorldConnection swc = new SolverWorldConnection();
@@ -75,7 +86,7 @@ public class Main {
 				final long earliestTime = cal.getTimeInMillis();
 
 				final ArrayList<Attribute> attrs = genAttributes(numAttributes,
-						1, earliestTime);
+						1, earliestTime, expire);
 
 				final long startCreateAll = System.nanoTime();
 				swc.updateAttributes(attrs);
@@ -97,6 +108,14 @@ public class Main {
 						(endDeleteInd - endCreateAll), numAttributes,
 						(endCreateInd - endDeleteInd)));
 
+				if (expire) {
+					System.out.println("Expiring attributes");
+					for (Attribute a : attrs) {
+						swc.expire(a.getId(), a.getExpirationDate(),
+								a.getAttributeName());
+					}
+				}
+
 				System.out.println("Sleeping 5 seconds");
 				try {
 					Thread.sleep(5000);
@@ -112,22 +131,24 @@ public class Main {
 					Thread.yield();
 				}
 				final long endEntireRange = System.nanoTime();
-				System.out.println(String.format("RA: "
-						+ TIME_FORMAT, (endEntireRange - startEntireRange)));
-				
+				System.out.println(String.format("RA: " + TIME_FORMAT,
+						(endEntireRange - startEntireRange)));
+
 				long startTenPct = earliestTime;
 				// Now select 10% at a time
-				for(int i = 1; i < 11	; ++i){
-					final long endTenPct = (long)((latestTime-earliestTime)*(i/10.0)) + earliestTime;
+				for (int i = 1; i < 11; ++i) {
+					final long endTenPct = (long) ((latestTime - earliestTime) * (i / 10.0))
+							+ earliestTime;
 					final long start = System.nanoTime();
-					resp = cwc.getRangeRequest(".*", startTenPct, endTenPct, ".*");
-					while(!resp.isComplete()){
+					resp = cwc.getRangeRequest(".*", startTenPct, endTenPct,
+							".*");
+					while (!resp.isComplete()) {
 						Thread.yield();
 					}
 					final long end = System.nanoTime();
-					
+
 					int count = 0;
-					while(resp.hasNext()){
+					while (resp.hasNext()) {
 						try {
 							resp.next();
 						} catch (Exception e) {
@@ -136,33 +157,35 @@ public class Main {
 						}
 						++count;
 					}
-					
-					System.out.println(String.format("R10/%2d("+COUNT_FORMAT+": "
-							+ TIME_FORMAT, i,count,(end - start)));
-					
+
+					System.out.println(String.format("R10/%2d(" + COUNT_FORMAT
+							+ "): " + TIME_FORMAT, i, count, (end - start)));
+
 					startTenPct = endTenPct;
 				}
-				
-				System.out.println("Sleeping 5 seconds");
+
+				System.out.println("Sleeping 1 second");
 				try {
-					Thread.sleep(5000);
+					Thread.sleep(1000);
 				} catch (InterruptedException ie) {
 					// Ignored
 				}
 
 				// Now select 5% at a time
 				long start05Pct = earliestTime;
-				// Now select 10% at a time
-				for(int i = 1; i < 21; ++i){
-					final long end20Pct = (long)((latestTime-earliestTime)*(i/20.0)) + earliestTime;
+
+				for (int i = 1; i < 21; ++i) {
+					final long end05Pct = (long) ((latestTime - earliestTime) * (i / 20.0))
+							+ earliestTime;
 					final long start = System.nanoTime();
-					resp = cwc.getRangeRequest(".*", start05Pct, end20Pct, ".*");
-					while(!resp.isComplete()){
+					resp = cwc
+							.getRangeRequest(".*", start05Pct, end05Pct, ".*");
+					while (!resp.isComplete()) {
 						Thread.yield();
 					}
 					final long end = System.nanoTime();
 					int count = 0;
-					while(resp.hasNext()){
+					while (resp.hasNext()) {
 						try {
 							resp.next();
 						} catch (Exception e) {
@@ -171,40 +194,153 @@ public class Main {
 						}
 						++count;
 					}
-					System.out.println(String.format("R20/%2d("+COUNT_FORMAT+"): "
-							+ TIME_FORMAT, i,count,(end - start)));
-					
-					start05Pct = end20Pct;
-					
+					System.out.println(String.format("R05/%2d(" + COUNT_FORMAT
+							+ "): " + TIME_FORMAT, i, count, (end - start)));
+
+					start05Pct = end05Pct;
+
 				}
-				
-				// Now snapshots at 5% steps, starting from 2.5% -> 97.5% 
-				System.out.println("Sleeping 5 seconds");
+
+				System.out.println("Sleeping 1 second");
 				try {
-					Thread.sleep(5000);
+					Thread.sleep(1000);
 				} catch (InterruptedException ie) {
 					// Ignored
 				}
 
-				for(int i = 0; i < 20; ++i){
-					final long end05Snap = (long)((latestTime-earliestTime)*(2.5 + i/20.0)) + earliestTime;
+				// Now select 1% at a time
+				long start01Pct = earliestTime;
+
+				for (int i = 1; i < 101; ++i) {
+					final long end01Pct = (long) ((latestTime - earliestTime) * (i / 100.0))
+							+ earliestTime;
+					final long start = System.nanoTime();
+					resp = cwc
+							.getRangeRequest(".*", start01Pct, end01Pct, ".*");
+					while (!resp.isComplete()) {
+						Thread.yield();
+					}
+					final long end = System.nanoTime();
+					int count = 0;
+					while (resp.hasNext()) {
+						try {
+							resp.next();
+						} catch (Exception e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						}
+						++count;
+					}
+					System.out.println(String.format("R01/%2d(" + COUNT_FORMAT
+							+ "): " + TIME_FORMAT, i, count, (end - start)));
+
+					start01Pct = end01Pct;
+
+				}
+
+				// Now snapshots at 5% steps, starting from 2.5% -> 97.5%
+				System.out.println("Sleeping 1 second");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException ie) {
+					// Ignored
+				}
+
+				for (int i = 0; i < 20; ++i) {
+					final long end05Snap = (long) ((latestTime - earliestTime) * (2.5 + i / 20.0))
+							+ earliestTime;
 					final long start = System.nanoTime();
 					WorldState state = null;
 					try {
-						state = cwc.getSnapshot(".*", end05Snap, end05Snap, ".*").get();
+						state = cwc.getSnapshot(".*", end05Snap, end05Snap,
+								"a.*").get();
 					} catch (Exception e1) {
 						// TODO Auto-generated catch block
 						e1.printStackTrace();
 					}
 					final long end = System.nanoTime();
 					int count = 0;
-					if(null != state){
-						count = state.getIdentifiers().size();
+					if (null != state) {
+						for(String id : state.getIdentifiers()){
+							Collection<Attribute> as = state.getState(id);
+							count += as.size();
+						}
 					}
-					
-					System.out.println(String.format("S20/%2.1f(" + COUNT_FORMAT + "): "
-							+ TIME_FORMAT, 2.5+5*i,count,(end - start)));
-					
+
+					System.out.println(String.format("S/%2.1f("
+							+ COUNT_FORMAT + "): " + TIME_FORMAT, 2.5 + 5 * i,
+							count, (end - start)));
+
+				}
+
+				// Now URI search
+				System.out.println("Sleeping 1 second");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException ie) {
+					// Ignored
+				}
+
+				for (int i = 0; i < SEARCH_ITERATIONS; ++i) {
+					final long start = System.nanoTime();
+					String[] ids = cwc.searchId(REGEX_PATTERN_SIMPLE);
+					final long end = System.nanoTime();
+
+					System.out.println(String.format("Rx/S(" + COUNT_FORMAT
+							+ "): " + TIME_FORMAT, ids.length, (end - start)));
+				}
+
+				System.out.println("Sleeping 1 seconds");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException ie) {
+					// Ignored
+				}
+
+				for (int i = 0; i < SEARCH_ITERATIONS; ++i) {
+					final long start = System.nanoTime();
+					String[] ids = cwc.searchId(REGEX_PATTERN_MEDIUM);
+					final long end = System.nanoTime();
+
+					System.out.println(String.format("Rx/M(" + COUNT_FORMAT
+							+ "): " + TIME_FORMAT, ids.length, (end - start)));
+				}
+
+				System.out.println("Sleeping 1 seconds");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException ie) {
+					// Ignored
+				}
+
+				for (int i = 0; i < SEARCH_ITERATIONS; ++i) {
+					final long start = System.nanoTime();
+					String[] ids = cwc.searchId(REGEX_PATTERN_COMPLEX);
+					final long end = System.nanoTime();
+
+					System.out.println(String.format("Rx/C(" + COUNT_FORMAT
+							+ "): " + TIME_FORMAT, ids.length, (end - start)));
+				}
+
+				System.out.println("Sleeping 1 seconds");
+				try {
+					Thread.sleep(1000);
+				} catch (InterruptedException ie) {
+					// Ignored
+				}
+
+				for (int i = 0; i < SEARCH_ITERATIONS; ++i) {
+					final long start = System.nanoTime();
+					String[] ids = cwc.searchId(REGEX_PATTERN_COMPLEX_HI_EMPTY);
+					int count = 0;
+					if (ids != null) {
+						count = ids.length;
+					}
+
+					final long end = System.nanoTime();
+
+					System.out.println(String.format("Rx/C_(" + COUNT_FORMAT
+							+ "): " + TIME_FORMAT, count, (end - start)));
 				}
 
 			}
@@ -217,11 +353,11 @@ public class Main {
 
 	public static void printUsage() {
 		System.err
-				.println("<WM HOST> <SOLVER PORT> <CLIENT PORT> [--createAttributes <NUM ATTRS>] [--time <TIMESTAMP1> <TIMESTAMP2>]");
+				.println("<WM HOST> <SOLVER PORT> <CLIENT PORT> [--createAttributes <NUM ATTRS>] [--expire]");
 	}
 
 	public static ArrayList<Attribute> genAttributes(int idCount,
-			int attrPerId, final long startTime) {
+			int attrPerId, final long startTime, final boolean expireEach) {
 		ArrayList<Attribute> attrs = new ArrayList<Attribute>(idCount
 				* attrPerId);
 		final int numMinutes = attrs.size();
@@ -233,7 +369,7 @@ public class Main {
 			for (int j = 0; j < attrPerId; ++j) {
 				final Attribute a = new Attribute();
 				attrs.add(a);
-				a.setId(String.format("id_%09d", i));
+				a.setId(String.format("id_%030d", i));
 				a.setAttributeName(String.format(ATTRIBUTE_FORMAT, j));
 				a.setData(IntegerConverter.get().encode(
 						Integer.valueOf(RAND.nextInt())));
@@ -241,6 +377,9 @@ public class Main {
 				a.setOriginName(ORIGIN_STRING);
 				a.setCreationDate(cal.getTimeInMillis());
 				cal.add(Calendar.MINUTE, 1);
+				if (expireEach) {
+					a.setExpirationDate(cal.getTimeInMillis());
+				}
 			}
 		}
 
